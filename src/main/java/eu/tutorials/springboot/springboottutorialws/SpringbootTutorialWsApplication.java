9f18fe0e7a6ec6181b1d6fd6700ce4c49718d535
funnzz <dumitru.aa@gmail.com>
2020-11-22 01:27:57 +0200

    initial commit

---
+++ src/main/java/eu/tutorials/springboot/springboottutorialws/SpringbootTutorialWsApplication.java
@@ -0,0 +1,14 @@
+package eu.tutorials.springboot.springboottutorialws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTutorialWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTutorialWsApplication.class, args);
+		System.out.println("### APP IS UP!!!! ###");
+	}
+
+}
---
+++ src/test/java/eu/tutorials/springboot/springboottutorialws/SpringbootTutorialWsApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.tutorials.springboot.springboottutorialws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTutorialWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
