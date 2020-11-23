142a360034f6313cdf1cd6015402eccb052dba41
funnzz <dumitru.aa@gmail.com>
2020-11-23 23:17:54 +0200

    returning a response json/xml

--- src/main/java/eu/tutorials/springboot/springboottutorialws/ui/controllers/UserController.java
+++ src/main/java/eu/tutorials/springboot/springboottutorialws/ui/controllers/UserController.java
@@ -1,6 +1,12 @@
 package eu.tutorials.springboot.springboottutorialws.ui.controllers;
 
+import eu.tutorials.springboot.springboottutorialws.ui.model.response.UserRest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+
 
 @RestController
 @RequestMapping("/users")
@@ -9,12 +15,21 @@ public class UserController {
     @GetMapping
     public String getUsers(@RequestParam(value = "page") int page, @RequestParam(value = "limit", defaultValue = "2") int limit,
                            @RequestParam(value = "sort", required = false) String sort) {
-        return "get users was called with page "+ page +" and limit " + limit + " and sort " + sort;
+        return "get users was called with page " + page + " and limit " + limit + " and sort " + sort;
     }
 
-    @GetMapping(path = "/{userId}")
-    public String getUser(@PathVariable String userId) {
-        return "get user was called with userId: " + userId;
+    @GetMapping(path = "/{userId}", produces = {
+            MediaType.APPLICATION_JSON_VALUE,
+            MediaType.APPLICATION_XML_VALUE
+    })
+    public ResponseEntity<UserRest> getUser(@PathVariable String userId) {
+
+        UserRest returnValue = new UserRest();
+        returnValue.setEmail("aaa@mail.com");
+        returnValue.setFirstName("rocky");
+        returnValue.setLasttName("rambo");
+
+        return new ResponseEntity<UserRest>(returnValue,HttpStatus.OK);
     }
 
     @PostMapping
---
+++ src/main/java/eu/tutorials/springboot/springboottutorialws/ui/model/response/UserRest.java
@@ -0,0 +1,41 @@
+package eu.tutorials.springboot.springboottutorialws.ui.model.response;
+
+public class UserRest {
+
+    private String firstName;
+    private String lasttName;
+    private String email;
+    private String userId;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLasttName() {
+        return lasttName;
+    }
+
+    public void setLasttName(String lasttName) {
+        this.lasttName = lasttName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+}
