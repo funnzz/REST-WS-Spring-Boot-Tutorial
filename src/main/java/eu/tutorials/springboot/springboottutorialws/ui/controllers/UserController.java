a69ecc55c4ab3b5eb6eb146537f1928a041b16d0
funnzz <dumitru.aa@gmail.com>
2020-11-23 22:23:57 +0200

    handle GET request

--- src/main/java/eu/tutorials/springboot/springboottutorialws/ui/controllers/UserController.java
+++ src/main/java/eu/tutorials/springboot/springboottutorialws/ui/controllers/UserController.java
@@ -6,23 +6,29 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/users")
 public class UserController {
 
-    @GetMapping()
-    public String getUser(){
-        return "get user was called";
+    @GetMapping
+    public String getUsers(@RequestParam(value = "page") int page, @RequestParam(value = "limit", defaultValue = "2") int limit,
+                           @RequestParam(value = "sort", required = false) String sort) {
+        return "get users was called with page "+ page +" and limit " + limit + " and sort " + sort;
+    }
+
+    @GetMapping(path = "/{userId}")
+    public String getUser(@PathVariable String userId) {
+        return "get user was called with userId: " + userId;
     }
 
     @PostMapping
-    public String createUser(){
+    public String createUser() {
         return "create User was called";
     }
 
     @PutMapping
-    public String updateUser(){
+    public String updateUser() {
         return "update user was called";
     }
 
     @DeleteMapping
-    public String deleteUser(){
+    public String deleteUser() {
         return "delete user was called";
     }
 
